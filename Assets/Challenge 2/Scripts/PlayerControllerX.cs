e7f902b459f3ba7af58ad94b47a040a404267f2d
ZEBAYA <119729932+ZEBAYA@users.noreply.github.com>
2023-09-16 15:01:30 +0300

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delayTime = 1;
+    private float startTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > startTime)
+        {
+            //Spamming the space bar
+            startTime = Time.time + delayTime;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
